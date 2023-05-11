4a03de924d32664d3f07f334ccfbc63649af447f
Gabriel Machado <gabriel.mach@hotmailcom>
2023-05-11 15:24:29 -0300

    Teste React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+      <>
+      <h1 className='titulo'>Chegou a patroa!</h1>
+      <img className='foto'src="https://thumbs.gfycat.com/FoolhardyRightGermanshorthairedpointer-size_restricted.gif"/>
+      </>
+    )
+}
+
+export default Home;
